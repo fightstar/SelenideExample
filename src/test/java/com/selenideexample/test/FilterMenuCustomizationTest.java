6dd0d982d1b559bd4d14ba2f3390ef8cbb671a8d
fightstar <vladislav.shevchenko@gmail.com>
2013-10-07 18:57:57 +0300

    Work commits

---
+++ src/test/java/com/selenideexample/test/FilterMenuCustomizationTest.java
@@ -0,0 +1,11 @@
+package com.selenideexample.test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: vshevchenko
+ * Date: 07.10.13
+ * Time: 18:54
+ * To change this template use File | Settings | File Templates.
+ */
+public class FilterMenuCustomizationTest {
+}
